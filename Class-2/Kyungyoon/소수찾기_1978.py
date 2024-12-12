fbe7aff057438e674cd83119ec225a23e5c7cf2e
kkyungyoon <ruddbs803@gmail.com>
2024-12-12 10:29:37 +0900

    1212

--- Class-2/Kyungyoon/소수찾기_1978.py
+++ Class-2/Kyungyoon/소수찾기_1978.py
@@ -4,7 +4,7 @@ num_list = list(map(int, input().split()))
 
 ans = 0
 for num in num_list:
-    for i in range(2, num//2 + 1):
+    for i in range(2, int(math.sqrt(num) + 1)):
         if num % i == 0:
             break
     else:
