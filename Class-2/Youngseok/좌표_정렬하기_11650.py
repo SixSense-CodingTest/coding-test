231e0dd47adc32be4f0893979a622543b8d83d3f
kimyoungseok3232 <rpfps3232@gmail.com>
2024-12-16 15:17:43 +0900

    solve: add 좌표 정렬하기

--- Class-2/Youngseok/좌표_정렬하기_11650.py
+++ Class-2/Youngseok/좌표_정렬하기_11650.py
@@ -1,6 +1,26 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
+p = [input() for _ in range(n)]
+p.sort(key = lambda x: tuple(map(int, x.split())))
+print(''.join(p))
+# 55144 kb
+# 216 ms
+
+import sys
+input = sys.stdin.readline
+n = int(input())
+p = sorted([input() for _ in range(n)], key = lambda x: tuple(map(int, x.split())))
+print(''.join(p))
+# 55144 kb
+# 208 ms
+
+import sys
+input = sys.stdin.readline
+n = int(input())
 p = sorted([input() for _ in range(n)])
 p.sort(key = lambda x: tuple(map(int, x.split())))
 print(''.join(p))
+# 53432 kb
+# 176 ms
+# WHY???????
