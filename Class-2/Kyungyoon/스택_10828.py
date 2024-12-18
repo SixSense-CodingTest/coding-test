60c8580aa0b3d1c25aa102d9b9b900b86e55c475
kkyungyoon <ruddbs803@gmail.com>
2024-12-19 08:53:33 +0900

    solve: Add solution for problem solving challenges 괄호 숫자 카드 2 스택

---
+++ Class-2/Kyungyoon/스택_10828.py
@@ -0,0 +1,32 @@
+from collections import deque
+N = int(input())
+command = [list(input().split()) for _ in range(N)]
+
+dq = deque()
+for c in command:
+    if c[0] == 'push':
+        dq.append(c[1])
+    if c[0] == 'pop':
+        if len(dq) != 0:
+            p = dq.pop()
+            print(p)
+        else:
+            print(-1)
+    if c[0] == 'size':
+        print(len(dq))
+    if c[0] == 'empty':
+        if len(dq) != 0:
+            print(0)
+        else:
+            print(1)
+    if c[0] == 'top':
+        if len(dq) != 0:
+            print(dq[-1])
+        else:
+            print(-1)
+
+"""
+PyPy3
+메모리 : 114504 KB
+시간 : 156 ms
+"""
