5bed373bc751f2cd2c23c63b8cbdf0cb8addc86d
kimyoungseok3232 <rpfps3232@gmail.com>
2024-12-22 20:12:34 +0900

    solve: 12/22 ATM 듣보잡 피보나치 함수

---
+++ Class-3/Youngseok/듣보잡_1764.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int,input().split())
+nl = set(input() for _ in range(n))
+res = [i for _ in range(m) if (i := input()) in nl]
+print(len(res))
+print(''.join(sorted(res)))
+# 37532kb
+# 64ms
---
+++ Class-3/Youngseok/피보나치_함수_1003.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+for _ in range(int(input())):
+    n, a, b = int(input()), 1, 0
+    for j in range(n): a, b = b, a+b
+    print(a, b)
+# 32412kb
+# 32ms
