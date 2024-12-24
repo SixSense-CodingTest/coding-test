a1f45382a2c8ae872e7de3b41a0704e05d79afe6
kimyoungseok3232 <rpfps3232@gmail.com>
2024-12-24 10:04:34 +0900

    fix: 구간 합 구하기 4

--- Class-3/Youngseok/구간_합_구하기_4_11659.py
+++ Class-3/Youngseok/구간_합_구하기_4_11659.py
@@ -1,9 +1,8 @@
-
 import sys
 input = sys.stdin.readline
 print = sys.stdout.write
 n, m = map(int, input().split())
-l = [0]+list(map(int, input().split()))+[0]
+l = [0]+list(map(int, input().split()))
 for i in range(1,n+1): l[i] += l[i-1]
 for i in range(m):
     a, b = map(int, input().split())
