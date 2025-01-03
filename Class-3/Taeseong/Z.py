3a33f3f7da44d5a8ac4de0730642b574b761dbe9
Taeseong <randfo42@gmail.com>
2025-01-03 10:29:14 +0900

    solve: Add solution for problem solving challenges Z

--- Class-3/Taeseong/Z.py
+++ Class-3/Taeseong/Z.py
@@ -1,18 +1,19 @@
-N,r,c = list(map(int,input().split()))
+N, y, x = map(int, input().split())
 
+def rec(N, y, x, T):
+    if N == 1:
+        return T + 2 * y + x
 
-def rec(N,r,c,T):
-    print(N,r,c,T)
-    if N==1:
-        return T + 2*r + c
-    
-    if r < 2**(N-1) and c < 2**(N-1): # 1 
-        return rec(N-1,r,c,T)
-    elif r >= 2**(N-1) and c < 2**(N-1): # 2 
-        return rec(N-1,r-2**(N-1),c,T+2**(2*N-2))
-    elif r < 2**(N-1) and c >= 2**(N-1): # 3 
-        return rec(N-1,r,c-2**(N-1),T+2*2**(2*N-2))
-    else:
-        return rec(N-1,r-2**(N-1),c-2**(N-1),T+3*2**(2*N-2))
+    size = 2 ** (N - 1)
+    quarter_area = size * size
 
-print(rec(N,c,r,0))
+    if y < size and x < size:  # 1
+        return rec(N - 1, y, x, T)
+    elif y < size and x >= size:  # 2
+        return rec(N - 1, y , x - size, T + quarter_area)
+    elif y >= size and x < size:  # 3
+        return rec(N - 1, y - size, x , T + 2 * quarter_area)
+    else:  # 4
+        return rec(N - 1, y - size, x - size, T + 3 * quarter_area)
+
+print(rec(N, y, x, 0))
