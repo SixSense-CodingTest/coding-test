97edb0c81a137333c6d1ca6e80a7d2d1e6110be3
kimyoungseok3232 <rpfps3232@gmail.com>
2025-01-06 08:17:57 +0900

    solve: 01/06 RGB거리

---
+++ Class-4/Youngseok/RGB거리_1149.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+v = [0, 0, 0]
+for _ in range(int(input())):
+    r, g, b = map(int, input().split())
+    v[0], v[1], v[2] = min(v[1], v[2]) + r, min(v[0], v[2]) + g, min(v[0], v[1]) + b
+print(min(v))
+# 32412kb
+# 40ms
