668246ca80fb13458a15b2c05a783af69b227953
kimyoungseok3232 <rpfps3232@gmail.com>
2025-01-07 16:05:02 +0900

    solve: 01/07 가장 긴 증가하는 부분 수열

--- Class-4/Youngseok/가장 긴 증가하는 부분 수열_11053.py
+++ Class-4/Youngseok/가장 긴 증가하는 부분 수열_11053.py
@@ -21,3 +21,15 @@ print(max(l))
 # 32412kb
 # 44ms
 ## 키 값에 부분 수열의 길이가 들어가고 밸류에 같은 길이의 부분 수열의 끝 값 중 가장 작은 값
+
+n = int(input())
+l = {0:0}
+for i in map(int, input().split()):
+    for j in range(len(l)-1,-1,-1):
+        if l[j] < i :
+            if j+1 not in l or l[j+1] > i :
+                l[j+1] = i
+                break
+print(len(l)-1)
+# 32412kb
+# 40ms
