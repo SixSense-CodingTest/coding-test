3f84eda30038a0e0bcaef34eff5564877960c2ba
kimyoungseok3232 <rpfps3232@gmail.com>
2025-01-09 04:01:15 +0900

    solve: 01/09 숨바꼭질 3

---
+++ Class-4/Youngseok/숨바꼭질 3_13549.py
@@ -0,0 +1,21 @@
+from collections import deque
+n, k = map(int, input().split())
+v = [1] * 100001
+vd = [1] * 100001
+q = deque([(n, 0)])
+while q:
+    t, c = q.popleft()
+    while t < 100001 and vd[t]:
+        if t == k: break
+        v[t] = 0
+        vd[t] = 0
+        for i in [t+1, t-1]:
+            if -1 < i < 100001 and v[i]:
+                v[i] = 0
+                q.append((i, c+1))
+        t *= 2
+    else: continue
+    break
+print(c)
+# 36024kb
+# 132ms
