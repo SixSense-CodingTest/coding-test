ee43be31c86c2bf1785d2c2c36cb5ee004d97fd0
kimyoungseok3232 <rpfps3232@gmail.com>
2025-01-10 20:38:33 +0900

    solve: 01/10 최단경로 트리의 지름

---
+++ Class-4/Youngseok/최단경로_1753.py
@@ -0,0 +1,54 @@
+import sys
+from heapq import heappush, heappop
+input = sys.stdin.readline
+n, edge = map(int, input().split())
+start = int(input())
+graph = [[] for _ in range(n+1)]
+for i in range(edge):
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w))
+
+dist = [float('INF')] * (n + 1)
+dist[start] = 0
+heap = [(0, start)]
+
+while heap:
+    current_dist, node = heappop(heap)
+    if current_dist > dist[node]: continue
+    for v, w in graph[node]:
+        if current_dist + w < dist[v]:
+            dist[v] = current_dist + w
+            heappush(heap, (dist[v], v))
+
+print(*map(lambda x: f'{x:.0F}',dist[1:]))
+# 70516kb
+# 688ms
+
+import sys
+from heapq import heappush, heappop
+input = sys.stdin.readline
+print = sys.stdout.write
+n, edge = map(int, input().split())
+start = int(input())
+graph = [[] for _ in range(n+1)]
+for i in range(edge):
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w))
+
+dist = [10e9] * (n + 1)
+dist[start] = 0
+heap = [(0, start)]
+
+while heap:
+    current_dist, node = heappop(heap)
+    if current_dist > dist[node]: continue
+    for v, w in graph[node]:
+        if current_dist + w < dist[v]:
+            dist[v] = current_dist + w
+            heappush(heap, (dist[v], v))
+
+for i in range(1, n+1):
+    if dist[i] == 10e9: print('INF\n')
+    else: print(f'{dist[i]}\n')
+# 69300kb
+# 640ms
---
+++ Class-4/Youngseok/트리의 지름_1967.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+graph = [[] for _ in range(n+1)]
+for _ in range(n-1):
+    parent, child, weight = map(int, input().split())
+    graph[parent].append((child, weight))
+    graph[child].append((parent, weight))
+
+def maxdist(start):
+    distance = [-1] * (n+1)
+    distance[start] = 0
+    q = [start]
+    while q:
+        node = q.pop()
+        for neighbor, weight in graph[node]:
+            if distance[neighbor] == -1:
+                distance[neighbor] = distance[node] + weight
+                q.append(neighbor)
+    max_dist = max(distance)
+    max_idx = distance.index(max_dist)
+    return max_dist, max_idx
+
+print(maxdist(maxdist(1)[1])[0])
+# 35480kb
+# 56ms
