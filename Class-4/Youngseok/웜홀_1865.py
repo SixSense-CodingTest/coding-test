08facb79168166448d112b4400eccdcc833a3656
kimyoungseok3232 <rpfps3232@gmail.com>
2025-01-23 23:51:16 +0900

    solve: 01/23 웜홀

---
+++ Class-4/Youngseok/웜홀_1865.py
@@ -0,0 +1,31 @@
+def solve(n):
+    for _ in range(n):
+        node, edge, hole = map(int, input().split())
+        edges = [[] for _ in range(node+1)]
+        graph = [[0]*(node+1) for _ in range(node+1)]
+        wh_st = set()
+        for _ in range(edge):
+            s, e, t = map(int, input().split())
+            edges[s].append((e,t))
+            edges[e].append((s,t))
+        for _ in range(hole):
+            s, e, t = map(int, input().split())
+            edges[s].append((e,-t))
+            graph[s][e] = -t
+            wh_st.add((s,e))
+
+        while wh_st:
+            st, mid = wh_st.pop()
+            for ed, time in edges[mid]:
+                if graph[st][mid] + time < graph[st][ed]:
+                    graph[st][ed] = graph[st][mid] + time
+                    wh_st.add((st, ed))
+                    if st == ed:
+                        print('YES')
+                        break
+            else: continue
+            break
+        else: print('NO')
+solve(int(input()))
+# 36336kb
+# 152ms
